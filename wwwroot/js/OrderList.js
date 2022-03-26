d1f76db46e761c942252ee1d6448939ecfd262cc
DanielVillatoro <danielvillatoroc@gmail.com>
2022-03-26 06:11:05 -0600

    Se realiza el item 4.

---
+++ wwwroot/js/OrderList.js
@@ -0,0 +1,34 @@
+﻿const GetList = () => {
+    let arrayInt = [];
+    let htmlData = "";
+    var element;
+    for (var i = 0; i < 20; i++) {
+        element = Math.trunc(Math.random() * (101 - 1) + 1)
+        htmlData += ' <li class="list-group-item">' + element +'</li>';
+        arrayInt.push(element);
+    }
+    $("#firtsList").html(htmlData);
+    for (var x = 0; x < arrayInt.length; x++) {
+        var value = arrayInt[x];
+        for (var j = x - 1; j >= 0 && (arrayInt[j] > value); j--) {
+            arrayInt[j + 1] = arrayInt[j];
+        }
+        arrayInt[j + 1] = value;
+    }
+    htmlData = "";
+    for (var k = 0; k < arrayInt.length; k++) {
+        htmlData += ' <li class="list-group-item">' + arrayInt[k] + '</li>';
+    }
+    $("#secondList").html(htmlData);
+    console.log(arrayInt);
+    //const colTotal = $("#colInput").val();
+    //var htmlData = "<tr>";
+    //for (var i = 0; i < colTotal; i++) {
+    //    htmlData += "<th scope='col'>";
+    //    htmlData += "Col-" + i;
+    //    htmlData += "</th>";
+    //}
+    //htmlData += "</tr>";
+    //$("#theadTable").html(htmlData);
+    //console.log(Math.trunc(Math.random() * (101 - 1) + 1));
+}
