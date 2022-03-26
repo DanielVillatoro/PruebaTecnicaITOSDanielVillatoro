109c00c4ec393b16e34b01b579f841ce43bdd38e
DanielVillatoro <danielvillatoroc@gmail.com>
2022-03-25 22:26:26 -0600

    Agregar archivos de proyecto.

---
+++ wwwroot/js/Bisiesto.js
@@ -0,0 +1,22 @@
+﻿$("#yearInput").val(new Date().getFullYear());
+
+const GetResult = () => {
+    const year = $("#yearInput").val();
+    if (verifiedYear(year)) {
+        $("#resultInput").val("Es un año bisiesto.")
+    }
+    else {
+        $("#resultInput").val("No es un año bisiesto.")
+    }
+}
+
+const verifiedYear = (year) => {
+    return (year % 400 === 0) ? true :
+        (year % 100 === 0) ? false :
+            year % 4 === 0;
+};
+
+
+const cleanInput = () => {
+    $("#resultInput").val("");
+}
